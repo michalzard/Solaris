a35925bd3f8a2fb18053fc448accc4ade858a8c5
MichalPlatko <miso.platko@gmail.com>
2021-05-12 01:30:59 +0200

    init stats

--- src/components/Statistics/ServerStats.jsx
+++ src/components/Statistics/ServerStats.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./ServerStats.scss";
+// import PlayerList from "./PlayerList";
+import PerformanceChart from './PerformanceChart';
 
 function ServerStats({ws}) {
     return (
         <div className="server-stats">
-            server stats page
+           <PerformanceChart/>
         </div>
     )
 }
