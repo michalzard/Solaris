b0f6a3080fac26f2497e79e3f95e1b66426c91d8
MichalPlatko <miso.platko@gmail.com>
2021-05-11 13:05:41 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./styles/App.scss";
-import Console from "./components/Console";
 import {useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Console from "./components/Console";
+import Status from "./components/Status";
+import Navbar from "./components/Navbar";
+import ServerInfo from "./components/ServerInfo";
+import ServerConfig from "./components/ServerConfig";
+import ServerStats from "./components/ServerStats";
 
 function App() {
   let ws=new WebSocket(`ws://${process.env.REACT_APP_IP}:${process.env.REACT_APP_PORT}/${process.env.REACT_APP_PW}`);
@@ -14,7 +25,24 @@ function App() {
 });
   return (
     <div className="App">
+      <Router>
+      <Navbar/>
+      <Switch>
+     <Route path="/console">
      <Console ws={ws}/>
+     </Route>
+     <Route path="/stats">
+     <ServerStats ws={ws}/>
+     </Route>
+     <Route path="/info">
+     <ServerInfo ws={ws}/>
+     </Route>
+     <Route path="/config">
+     <ServerConfig ws={ws}/>
+     </Route>
+     </Switch>
+     <Status/>
+     </Router>
     </div>
   );
 }
