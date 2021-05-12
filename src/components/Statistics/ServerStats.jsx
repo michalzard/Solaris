409917ee2e501a974c52a5dfece60bb225ea496e
MichalPlatko <miso.platko@gmail.com>
2021-05-12 14:15:55 +0200

    init

--- src/components/Statistics/ServerStats.jsx
+++ src/components/Statistics/ServerStats.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./ServerStats.scss";
-// import PlayerList from "./PlayerList";
+import {Button} from "@material-ui/core";
 import PerformanceChart from './PerformanceChart';
+import StatCard from './StatCard';
 
 function ServerStats({ws}) {
     return (
         <div className="server-stats">
-           <PerformanceChart/>
+            <LeftSideBar/>
+            <div className="content">
+           <PerformanceChart color={{r:200,g:150,b:100}}
+           subtitle="Total Players" title="Players" fill/>
+            <StatCard/>
+           </div>
         </div>
     )
 }
 
+function LeftSideBar(){
+    return(
+        <div className="leftsidebar" style={{color:"white"}}>
+        <Button variant="contained" color="secondary">Stats</Button>
+        </div> 
+    )
+}
 export default ServerStats
