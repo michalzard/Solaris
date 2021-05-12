44acdd68afc59a36a8c18aa46866bf06c9f778c1
MichalPlatko <miso.platko@gmail.com>
2021-05-12 14:15:43 +0200

    chart component that display's historical data,sorted by daily,monthly,yearly

--- src/components/Statistics/PerformanceChart.jsx
+++ src/components/Statistics/PerformanceChart.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./PerformanceChart.scss";
 import {Chart, Line} from "react-chartjs-2";
 import {Button,ButtonGroup} from "@material-ui/core";
 Chart.defaults.plugins.legend.display=false;
 
-function PerformanceChart() {
+function PerformanceChart({subtitle,title,color,fill,data}) {
     const highlightRed="#f50057";
+    const [sortBy,setSortBy]=useState("Monthly");
+    const labels={
+    Daily:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    Monthly:["Jan","Feb","Mar","Apr","May","Jun","Sep","Oct","Nov","Dec"],
+    Yearly:["2020",new Date().getFullYear()],
+    }
     return (
         <div className="performanceChart">
         <div className="chart_info">
-        <span className="title">Performance</span>
-        <span className="buttons">
+        <div className="titles">
+        <span className="subtitle">{subtitle ? subtitle : "Total Performance"}</span>
+        <span className="title">{title ? title : "Performance"}</span>
+        </div>
+
+        <div className="buttons">
         <ButtonGroup color="secondary" aria-label="outlined primary button group">
-        <Button>Daily</Button>
-        <Button>Monthly</Button>
-        <Button>Yearly</Button>
+        <Button style={{backgroundColor:sortBy==="Daily" ? highlightRed : null,
+        color:sortBy==="Daily" ? "white" : null}} onClick={()=>{setSortBy("Daily")}}>Daily</Button>
+        <Button style={{backgroundColor:sortBy==="Monthly" ? highlightRed : null,
+        color:sortBy==="Monthly" ? "white" : null}} onClick={()=>{setSortBy("Monthly")}}>Monthly</Button>
+        <Button style={{backgroundColor:sortBy==="Yearly" ? highlightRed : null,
+        color:sortBy==="Yearly" ? "white" : null}} onClick={()=>{setSortBy("Yearly")}}>Yearly</Button>
         </ButtonGroup>
-        </span>
+        </div>
         </div>
 
         <div className="graph">
         <Line
         data={{
-        labels:["Jan","Feb","Mar","Apr","May","Jun","Sep","Oct","Nov","Dec"],
+        labels:labels[sortBy],
         datasets:[
             {
-                label: "Players",
-                data:[12,19,3,5,2,32,10,12,19,3,5,2,32,10],
-                borderColor:"rgba(250, 150, 50, 1)",
-                backgroundColor:"rgba(250, 150, 50,0.1)",
-                fill:true,
-                tension:.2
+                data: data ? data  : [50,100,70,35,50,55,5,10,15,20,25],
+                borderColor: color ? `rgba(${color.r}, ${color.g},${color.b}, 1)` : "rgba(255,0,0,1)",
+                backgroundColor:color ? `rgba(${color.r}, ${color.g},${color.b},.1)` : "rgba(255,0,0,.1)",
+                fill:fill ? true : false,
+                tension:.2,
             }
         ]
         }}
-        width={200}
+        width={400}
         height={200}
         options={{
         maintainAspectRatio:false,
