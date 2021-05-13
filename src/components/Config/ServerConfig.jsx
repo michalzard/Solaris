67523cdd3690151a7596f78d015fbe079a77c37c
MichalPlatko <miso.platko@gmail.com>
2021-05-13 12:33:10 +0200

    new dir

---
+++ src/components/Config/ServerConfig.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServerConfig({ws}) {
+    return (
+        <div className="server-config"> 
+            config page
+        </div>
+    )
+}
+
+export default ServerConfig
---
+++ src/components/Info/ServerInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServerInfo({ws}) {
+    return (
+        <div className="server-info">
+            server info page
+        </div>
+    )
+}
+
+export default ServerInfo
