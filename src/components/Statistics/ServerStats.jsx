5dcb6a0d2a5d0a568290d168f8da9a7508029db3
MichalPlatko <miso.platko@gmail.com>
2021-05-13 03:03:09 +0200

    card shows concurrent users

--- src/components/Statistics/ServerStats.jsx
+++ src/components/Statistics/ServerStats.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./ServerStats.scss";
-import {Button} from "@material-ui/core";
+//import {Button} from "@material-ui/core";
 import PerformanceChart from './PerformanceChart';
 import StatCard from './StatCard';
+import { useState } from 'react';
+import PersonIcon from '@material-ui/icons/Person';
+import CachedIcon from '@material-ui/icons/Cached';
 
 function ServerStats({ws}) {
+    const [concurrentPlayers,setConcurrentPlayers]=useState(0);
+    ws.onmessage=(e)=>{
+        const data=JSON.parse(e.data);
+        if(data.Message.includes("users")){
+        const connectedPlayers=data.Message.split("\n")[1].replace("users","");
+        setConcurrentPlayers(connectedPlayers);
+        }else return;
+    }
+    useEffect(()=>{
+        setTimeout(()=>{
+        if(ws)ws.send(JSON.stringify({Message:"users", Name:"Console"}));
+        },200);    
+    },[ws]);
+    
+
+    const updateConcurrentInfo=()=>{
+    const playerCard=document.getElementById("playerCard");
+    if(playerCard && !playerCard.classList.contains("rotateAnimation")){
+    ws.send(JSON.stringify({Message:"users", Name:"Console"}));
+    playerCard.classList.add("rotateAnimation");
+    setTimeout(()=>{playerCard.classList.remove("rotateAnimation");},1100);
+    }
+    }
     return (
         <div className="server-stats">
             <LeftSideBar/>
             <div className="content">
            <PerformanceChart color={{r:200,g:150,b:100}}
            subtitle="Total Players" title="Players" fill/>
-            <StatCard/>
+            <StatCard title="Connected Players" id="playerCard" className="m-top" value={concurrentPlayers}  desc="Update now"
+            onClickCallback={updateConcurrentInfo} icon={<PersonIcon fontSize="large"/>} actionIcon={<CachedIcon/>}/>
            </div>
         </div>
     )
 }
+// TODO:Performance chart needs to be connected to database
+
 
 function LeftSideBar(){
     return(
         <div className="leftsidebar" style={{color:"white"}}>
-        <Button variant="contained" color="secondary">Stats</Button>
         </div> 
     )
 }
